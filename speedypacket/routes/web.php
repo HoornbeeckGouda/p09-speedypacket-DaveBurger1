b1d9c00ad1f760f459de9fb5613e778e2c11c74e
Dave Burger <45798@hoornbeeck.nl>
2025-11-27 10:13:20 +0100

    Welcome page

--- speedypacket/routes/web.php
+++ speedypacket/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/welcome', function () {
+    Log::info('Request received: ' . request()->method() . ' ' . request()->path());
+    return response()->json(['message' => 'Welcome to SpeedyPacket!']);
 });
